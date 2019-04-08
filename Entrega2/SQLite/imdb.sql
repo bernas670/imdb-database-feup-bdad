f7ed1aa6cf4aee575073aa7f7943e888ffb02500
David Silva <up201705373@fe.up.pt>
2019-04-08 19:22:47 +0100

    Some changes to sql

--- Entrega2/SQLite/imdb.sql
+++ Entrega2/SQLite/imdb.sql
@@ -13,10 +13,20 @@ CREATE TABLE Content(
 
 CREATE TABLE MOVIE(
 
-    movieID             
+    movieID             INT PRIMARY KEY REFERENCES Content(id),
+    year                INT,
+    boxoffice           REAL          
+
+);
 
 
+CREATE TABLE TVShow(
 
+    id                  INT PRIMARY KEY,
+    name                TEXT,
+    description         TEXT,
+    ageRating           TEXT,
+    score               INT
 
 );
 
@@ -25,7 +35,30 @@ CREATE TABLE Episode(
     episodeID           INT PRIMARY KEY REFERENCES Content(id),
     number              INT,
     seasonNumber        INT,
-    tvshowID            INT REFERENCES TVShow(id),
+    tvshowID            INT REFERENCES TVShow(id)
+
+);
+
+
+CREATE TABLE Award(
+
+    id                  INT PRIMARY KEY,
+    category            TEXT,
+    awardNameID         INT REFERENCES AwardName(id)
+        
+);
+
+CREATE TABLE AwardName(
+
+    id                  INT PRIMARY KEY,
+    name                TEXT
+
+);
+
+
+CREATE TABLE MovieAward(
+
+    year                INT,
 
 
 
