a0cf15e5b02c48a00872884acee9baedc0f097e1
Bernardo Santos <bernardosantos670@gmail.com>
2019-04-08 19:40:52 +0100

    Updated imdb.sql

--- Entrega2/SQLite/imdb.sql
+++ Entrega2/SQLite/imdb.sql
@@ -25,8 +25,49 @@ CREATE TABLE Episode(
     episodeID           INT PRIMARY KEY REFERENCES Content(id),
     number              INT,
     seasonNumber        INT,
-    tvshowID            INT REFERENCES TVShow(id),
+    tvshowID            INT REFERENCES TVShow(id)
 
 
 
+);
+
+CREATE TABLE TvShowReview(
+	tvshowID			INT REFERENCES TVShow(id),
+	userID				INT REFERENCES User(id),
+	score				INT,
+	review				TEXT,
+	PRIMARY KEY(tvshowID, userID)
+);
+
+CREATE TABLE Watchlist(
+	contentID			INT REFERENCES Content(id),
+	userID				INT REFERENCES User(id),
+	PRIMARY KEY(contentID, userID)
+);
+
+CREATE TABLE Gender(
+	id 					INT PRIMARY KEY,
+	name				TEXT
+);
+
+CREATE TABLE Country(
+	id					INT PRIMARY KEY,
+	name				TEXT
+);
+
+CREATE TABLE Released(
+	contentID			INT PRIMARY KEY REFERENCES Content(id),
+	date				TEXT,
+	countryID			INT REFERENCES Country(id)
+);
+
+CREATE TABLE Genre(
+	id					INT PRIMARY KEY,
+	name				TEXT,
+	description			TEXT
+);
+
+CREATE TABLE ContentGenre(
+	contentID			INT REFERENCES Content(id),
+	genreID				INT REFERENCES Genre(id)
 );
