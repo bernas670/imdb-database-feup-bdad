01c993cc65a04bc1d3c92a8a8787e6cef1f38ffc
luispcunha <luispplmcunha@gmail.com>
2019-04-14 12:59:44 +0100

    Created povoar.sql

--- Entrega2/SQLite/populate.sql
+++ Entrega2/SQLite/populate.sql
@@ -1,15 +1,32 @@
-PRAGMA foreign_keys = on;
+PRAGMA foreign_keys = ON;
 
-INSERT INTO Person(firstName, lastName, dateOfBirth)
-VALUES ('Robert', 'De Niro', '1943-08-17');
+INSERT Person (id, firstName, lastName, dateOfBirth) 
+INSERT INTO Person (id, firstName, lastName, dateOfBirth) VALUES ('Robert', 'De Niro', '1943-08-17');
+INSERT INTO Person (id, firstName, lastName, dateOfBirth) VALUES ('Samuel', 'L. Jackson', '1948-12-21');
+INSERT INTO Person (id, firstName, lastName, dateOfBirth, dateOfDeath) VALUES ('Marilyn', 'Monroe', '1926-06-01', '1962-08-05');
 
-INSERT INTO Person(firstName, lastName, dateOfBirth)
-VALUES ('Samuel', 'L. Jackson', '1948-12-21');
+INSERT INTO Content(id, name, duration) VALUES (1, 'Taxi Driver', 114);
+INSERT INTO Movie(id, year) VALUES (1, 1976);
 
-INSERT INTO Person(firstName, lastName, dateOfBirth, dateOfDeath)
-VALUES ('Marilyn', 'Monroe', '1926-06-01', '1962-08-05');
 
-INSERT INTO Content(id, name, duration)
-VALUES (1, 'Taxi Driver', 114);
-INSERT INTO Movie(id, year)
-vALUES (1, 1976);
+INSERT INTO Country (id,name) VALUES (1,"United States of America");
+INSERT INTO Country (id,name) VALUES (2,"Portugal");
+INSERT INTO Country (id,name) VALUES (3,"Australia");
+INSERT INTO Country (id,name) VALUES (4,"Italy");
+INSERT INTO Country (id,name) VALUES (5,"England");
+INSERT INTO Country (id,name) VALUES (6,"Scotland");
+INSERT INTO Country (id,name) VALUES (7,"Germany");
+INSERT INTO Country (id,name) VALUES (8,"Brazil");
+INSERT INTO Country (id,name) VALUES (9,"Mexico");
+INSERT INTO Country (id,name) VALUES (10,"France");
+INSERT INTO Country (id,name) VALUES (11,"Canada");
+INSERT INTO Country (id,name) VALUES (12,"Scotland");
+INSERT INTO Country (id,name) VALUES (13,"Angola");
+INSERT INTO Country (id,name) VALUES (14,"Belgium");
+INSERT INTO Country (id,name) VALUES (15,"South Africa");
+INSERT INTO Country (id,name) VALUES (16,"Venezuela");
+INSERT INTO Country (id,name) VALUES (17,"Egypt");
+INSERT INTO Country (id,name) VALUES (18,"Jamaica");
+INSERT INTO Country (id,name) VALUES (19,"Kazakhstan");
+INSERT INTO Country (id,name) VALUES (20,"Spain");
+
