d93851a3e3721fb35cf4a127e423f0bf8ccb909c
David Silva <up201705373@fe.up.pt>
2019-05-25 19:23:25 +0100

    Added query 3

---
+++ Entrega3/sqlite/queries/int3.sql
@@ -0,0 +1,14 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+
+SELECT Content.name as MovieName, ActorParticipationCount.PersonFN, ActorParticipationCount.PersonLN, max(cnt) as participationNumber
+FROM Content, (SELECT Movie.id AS MovieID,Person.id AS PersonID, Person.firstName AS PersonFN, Person.lastName AS PersonLN, count(Person.id) as cnt
+                FROM  Person,Movie,(SELECT * 
+                                    FROM RolePersonContent JOIN Role ON (RolePersonContent.roleID = Role.id) 
+                                    WHERE Role.description = 'Actor') AS ActorContent
+                WHERE Person.id = ActorContent.personID and Movie.id = ActorContent.contentID 
+                GROUP BY Person.id)    AS ActorParticipationCount   
+WHERE Content.id = ActorParticipationCount.MovieID
+GROUP BY ActorParticipationCount.MovieID 
