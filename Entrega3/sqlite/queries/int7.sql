ca5e7516ef1670498098af9d1e44da88d1f2bb88
Bernardo Santos <bernardosantos670@gmail.com>
2019-05-26 18:23:07 +0100

    All queries and triggers are corrected

--- Entrega3/sqlite/queries/int7.sql
+++ Entrega3/sqlite/queries/int7.sql
@@ -8,4 +8,4 @@ WITH W as (SELECT *
                                  JOIN RolePersonContent ON (WatchlistWithUsernames.contentID = RolePersonContent.contentID ))
 SELECT W1.name as user1, W2.name as user2
 FROM (W as W1 JOIN W as W2 ON (W1.userID < W2.userID and W1.personID = W2.personID))
-GROUP BY user1,user
+GROUP BY user1,user2
