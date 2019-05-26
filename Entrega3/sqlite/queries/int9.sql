46b4e72e3fc964dc5ee0453d1030a9767c2f7e29
luispcunha <luispplmcunha@gmail.com>
2019-05-26 13:22:17 +0100

    Modified int9

--- Entrega3/sqlite/queries/int9.sql
+++ Entrega3/sqlite/queries/int9.sql
@@ -5,23 +5,24 @@
 SELECT (firstName || " " || lastName) AS name, description AS role, tvshowName AS tvshow, episodeName AS episode
 FROM (
         (
-         SELECT  firstName, lastName, Role.description, TVShow.id as tvshowid, Content.name AS episodeName, count(Episode.id) AS numPart
-         FROM Person, RolePersonContent, Role, Content, Episode, TVShow
-         ON Person.id = RolePersonContent.personID AND
-            Role.id = RolePersonContent.roleID AND 
-            Content.id = RolePersonContent.contentID AND
-            Content.id = Episode.id AND 
-            Episode.tvshowID = TVShow.id 
-         WHERE (Role.description = 'Actor' OR Role.description = 'Voice actor') 
-         GROUP BY Person.id, TVShow.id 
-        ) AS ParticipationsInTVShow
+            SELECT  firstName, lastName, Role.description, TVShow.id AS tvshowid, 
+                    Content.name AS episodeName, count(Episode.id) AS numPart
+            FROM Person, RolePersonContent, Role, Content, Episode, TVShow
+            ON Person.id = RolePersonContent.personID AND
+               Role.id = RolePersonContent.roleID AND 
+               Content.id = RolePersonContent.contentID AND
+               Content.id = Episode.id AND 
+               Episode.tvshowID = TVShow.id 
+            WHERE (Role.description = 'Actor' OR Role.description = 'Voice actor') 
+            GROUP BY Person.id, TVShow.id 
+        ) 
         JOIN 
         (
-         SELECT TVShow.id AS tvshowid, TVShow.name AS tvshowName, count(Episode.id) AS numEpisodes
-         FROM Episode, TVShow
-         ON Episode.tvshowID = TVShow.id
-         GROUP BY TVShow.id
-        ) AS EpisodesInTVShow
-        ON ParticipationsInTVShow.tvshowid = EpisodesInTVShow.tvshowid
-     )
+            SELECT TVShow.id AS tvshowid, TVShow.name AS tvshowName, count(Episode.id) AS numEpisodes
+            FROM Episode, TVShow
+            ON Episode.tvshowID = TVShow.id
+            GROUP BY TVShow.id
+        )
+        USING(tvshowid)
+      )
 WHERE numPart = 1 AND numEpisodes > 1;
